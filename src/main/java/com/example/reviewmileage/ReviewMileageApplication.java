d588b640e1655d6dfae9380a4795802a5e57327b
seonb2n <626ksb@gmail.com>
2022-06-14 09:20:58 +0900

    pjt create

---
+++ src/main/java/com/example/reviewmileage/ReviewMileageApplication.java
@@ -0,0 +1,13 @@
+package com.example.reviewmileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewMileageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewMileageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reviewmileage/ReviewMileageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reviewmileage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewMileageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
